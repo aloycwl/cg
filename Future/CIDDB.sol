1c2ae02c3074ef726814b05a0107fe7076fca33e
aloycwl <aloycwl@gmail.com>
2023-09-06 12:58:35 +0800

    CIDDB done

--- Future/CIDDB.sol
+++ Future/CIDDB.sol
@@ -16,33 +16,34 @@ which will return the string.
 
 contract CIDDB {
     bytes32 constant private CDB = 0x34b90c3fe4058816a5fd62fd112c01472c461559e126623d04d1af72d9ad437e;
-    /*mapping(string => uint) public search;
-    mapping(uint => string) public fetch;
-    
-    bytes32 constant private FET = 0x6c1ac4b2af2ca57a4d77eb6a4b0bffbc328fc8319414baf27e6210b191a852a8;
-
-    function setFetch(string memory cid) external returns(uint nwc) {
-        if (search[cid] > 0) return search[cid];
-        assembly {
-            nwc := add(sload(CDB), 1)
-            sstore(0x00, nwc)
-        }
-        search[cid] = nwc;
-        fetch[nwc] = cid;
-    }
 
     function count() external view returns(uint) {
         assembly {
             mstore(0x00, sload(CDB))
             return(0x00, 0x20)
         }
-    }*/
+    }
+
+    function search(string memory str) external view returns(uint) {
+        assembly {
+            mstore(0x00, sload(keccak256(add(0x20, str), mload(str))))
+            return(0x00, 0x20)
+        }
+    }
 
     function store(string memory str) external returns(uint nwc) {
         assembly {
+            // if(fet[str] > 0)
+            let hsh := keccak256(add(0x20, str), mload(str))
+            mstore(0x00, sload(hsh))
+            if gt(sload(hsh), 0x00) {
+                return(0x00, 0x20)
+            }
+
             // nwc = count++
             nwc := add(sload(CDB), 0x01)
             sstore(CDB, nwc)
+            sstore(hsh, nwc)
 
             // str(length)
             mstore(0x00, nwc)
@@ -67,17 +68,15 @@ contract CIDDB {
             mstore(0xa0, sload(ptr))
             let cnt := 0x40
 
-            //mstore(0xc0, "abcdefghi")
+            // fetch each line
             for { let i := 0x01 } lt(mul(0x20, sub(i, 0x01)), sload(ptr)) { i := add(i, 0x01) } {
-                mstore(add(0xa0, mul(0x20, i)), sload(add(ptr, mul(i, 0x20))))
+                mstore(add(0x80, cnt), sload(add(ptr, i)))
                 cnt := add(0x20, cnt)
             }
-
 
             return(0x80, cnt)
         }
     }
 
-    
 }
 
