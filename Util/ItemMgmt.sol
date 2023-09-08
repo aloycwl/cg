545a78af3a6aa042ba501b2fb3c8dc8362e35515
aloycwl <aloycwl@gmail.com>
2023-09-08 13:24:54 +0800

    combined ItemMgmt and Sign

--- Util/ItemMgmt.sol
+++ Util/ItemMgmt.sol
@@ -2,10 +2,9 @@
 pragma solidity 0.8.19;
 pragma abicoder v1;
 
-import {Sign} from "../Util/Sign.sol";
 import {DynamicPrice} from "../Util/DynamicPrice.sol";
 
-contract ItemMgmt is Sign, DynamicPrice {
+contract ItemMgmt is DynamicPrice {
 
     bytes32 constant internal STO = 0x79030946dd457157e4aa08fcb4907c422402e75f0f0ecb4f2089cb35021ff964;
     bytes32 constant internal CNT = 0x5e423f2848a55862b54c89a4d1538a2d8aec99c1ee890237e17cdd6f0b5769d9;
@@ -150,6 +149,95 @@ contract ItemMgmt is Sign, DynamicPrice {
         pay(address(this), lis, this.owner(), 0); // 若金额设定就支付
         checkSuspend(msg.sender, msg.sender); // 查有被拉黑不
         check(lis, msg.sender, v, r, s); // 查签名
+    }
+
+    function check(uint amt, address adr, uint8 v, bytes32 r, bytes32 s) internal {
+
+        bytes32 hsh;
+        uint ind;
+
+        assembly {
+            // uintData(address(), addr, 0x1)
+            mstore(0x80, UIN) 
+            // 索取block.timestamp
+            mstore(0x84, address())
+            mstore(0xa4, adr)
+            mstore(0xc4, 0x01)
+            pop(staticcall(gas(), sload(STO), 0x80, 0x64, 0x00, 0x20))
+            ind := mload(0x00)
+            // 拿哈希信息
+            mstore(0x00, add(amt, add(adr, ind)))
+            mstore(0x00, keccak256(0x00, 0x20))
+            hsh := keccak256(0x00, 0x20)
+        }
+
+        address val = ecrecover(hsh, v, r, s);
+        
+        assembly {
+            let sto := sload(STO)
+            // addressData(0x0, 0x0, 0x1)；
+            mstore(0x80, ADR) 
+            // 索取signer
+            mstore(0x84, 0x00)
+            mstore(0xa4, 0x00)
+            mstore(0xc4, 0x01)
+            pop(staticcall(gas(), sto, 0x80, 0x64, 0x00, 0x20))
+            // require(ecrecover == signer)
+            if iszero(eq(val, mload(0x00))) {
+                mstore(0x80, ERR) 
+                mstore(0x84, 0x20)
+                mstore(0xA4, 0x07)
+                mstore(0xC4, "Sig err")
+                revert(0x80, 0x64)
+            }
+            // uintData(address(), addr, 0x1, ind++)
+            mstore(0x80, UID)
+            // 更新block.timestamp
+            mstore(0x84, address())
+            mstore(0xa4, adr)
+            mstore(0xc4, 0x01)
+            mstore(0xe4, add(0x01, ind))
+            pop(call(gas(), sto, 0x00, 0x80, 0x84, 0x00, 0x00))
+        }
+    }
+
+    function checkSuspend(address adr, address ad2) internal view {
+        assembly {
+            // 环这3个地址
+            mstore(0x80, address())
+            mstore(0xa0, adr)
+            mstore(0xc0, ad2)
+            // uintData(address()/a/b, 0x0, 0x0)
+            mstore(0xe0, UIN) 
+            mstore(0x0104, 0x00)
+            mstore(0x0124, 0x00)
+            for { let i := 0x00 } lt(i, 0x03) { i := add(i, 0x01) } {
+                // 地址是否被暂停
+                mstore(0xe4, mload(add(mul(i, 0x20), 0x80)))
+                pop(staticcall(gas(), sload(STO), 0xe0, 0x64, 0x00, 0x20))
+                // require(x == 0)
+                if gt(mload(0x00), 0x00) {
+                    mstore(0x80, ERR) 
+                    mstore(0x84, 0x20)
+                    mstore(0xA4, 0x17)
+                    mstore(0xC4, "Contract/user suspended")
+                    revert(0x80, 0x64)
+                }
+            }
+        }
+    }
+
+    // block.timestamp
+    function getUint(address adr, uint uid) external view returns(uint) { // 0x2c7fb918
+        assembly {
+            // uintData(address(), addr, 0x0)
+            mstore(0x80, UIN)
+            mstore(0x84, address())
+            mstore(0xa4, adr)
+            mstore(0xc4, uid)
+            pop(staticcall(gas(), sload(STO), 0x80, 0x64, 0x00, 0x20))
+            return(0x00, 0x20)
+        }
     }
 
 }
